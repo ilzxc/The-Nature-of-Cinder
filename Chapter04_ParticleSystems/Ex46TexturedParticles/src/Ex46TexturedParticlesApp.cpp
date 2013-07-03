bed949445807e7733e9d7762a7f92348222036d1
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-03 07:48:45 -0700

    Exercise 6: blue smoke

--- Chapter04_ParticleSystems/Ex46TexturedParticles/src/Ex46TexturedParticlesApp.cpp
+++ Chapter04_ParticleSystems/Ex46TexturedParticles/src/Ex46TexturedParticlesApp.cpp
@@ -1,5 +1,9 @@
 #include "cinder/app/AppNative.h"
+#include "Math.h"
 #include "cinder/gl/gl.h"
+#include "cinder/gl/Texture.h"
+#include "cinder/Rand.h"
+#include "Particle.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -8,27 +12,61 @@ using namespace std;
 class Ex46TexturedParticlesApp : public AppNative {
   public:
 	void setup();
-	void mouseDown( MouseEvent event );	
+	void mouseMove( MouseEvent event );
 	void update();
 	void draw();
+    void shutdown();
+    
+    gl::Texture image;
+    float windDirection, windSpeed;
+    Vec2f wind;
+    std::vector<Particle> particles;
+    Rand random;
 };
 
 void Ex46TexturedParticlesApp::setup()
 {
+    gl::enableAlphaBlending();
+    gl::enableAdditiveBlending();
+    random.randomize();
+    image = gl::Texture( loadImage( loadResource("texture00.png") ) );
+    particles.push_back( Particle( &image, Vec2f( getWindowWidth() / 2, getWindowHeight() * 0.7 ), random ) );
+    windSpeed = 0.11;
 }
 
-void Ex46TexturedParticlesApp::mouseDown( MouseEvent event )
-{
+void Ex46TexturedParticlesApp::mouseMove( MouseEvent event ){
 }
 
 void Ex46TexturedParticlesApp::update()
 {
+    windDirection += windSpeed;
+    if (windDirection >= 6.28318530717959) {
+        windSpeed = random.nextFloat(0.07, 0.23);
+        windDirection -= 6.28318530717959;
+    }
+    wind = Vec2f( sin(windDirection) * 0.0005f, 0.0f );
+    for(std::vector<Particle>::reverse_iterator iter = particles.rbegin(); iter != particles.rend(); ++iter) {
+        iter->update(wind);
+        if ( iter->isDead() ) {
+            particles.erase( --iter.base() );
+        }
+    }
+    
+    particles.push_back( Particle( &image, Vec2f( getWindowWidth() / 2, getWindowHeight() * 0.7  ), random ) );
+    
+    // std::cout << "Current particle count is " << particles.size() << std::endl;
 }
 
 void Ex46TexturedParticlesApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
+    
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i].draw();
+    }
+}
+
+void Ex46TexturedParticlesApp::shutdown() {
 }
 
 CINDER_APP_NATIVE( Ex46TexturedParticlesApp, RendererGl )
