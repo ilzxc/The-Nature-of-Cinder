9e2d60b70cc02d6eaacf4e72848fb47138f62112
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-03 07:48:45 -0700

    Exercise 6: blue smoke

---
+++ Chapter04_ParticleSystems/Ex46TexturedParticles/src/Particle.cpp
@@ -0,0 +1,31 @@
+//
+//  Particle.cpp
+//  Ex46TexturedParticles
+//
+//  Created by Ilya Rostovtsev on 7/3/13.
+//
+//
+
+#include "Particle.h"
+
+void Particle::update(Vec2f &force) {
+    acceleration += force;
+    velocity += acceleration;
+    position += velocity;
+    lifespan -= aging;
+}
+
+void Particle::draw() {
+    gl::color(0, lifespan * lifespan / 5.0, lifespan * lifespan * lifespan, lifespan);
+    gl::pushMatrices();
+    gl::translate(position);
+    gl::draw( *texture, Rectf(-sizeRadius, -sizeRadius, sizeRadius, sizeRadius));
+    gl::popMatrices();
+}
+
+bool Particle::isDead() {
+    if(lifespan < 0.0f) {
+        return true;
+    }
+    return false;
+}
