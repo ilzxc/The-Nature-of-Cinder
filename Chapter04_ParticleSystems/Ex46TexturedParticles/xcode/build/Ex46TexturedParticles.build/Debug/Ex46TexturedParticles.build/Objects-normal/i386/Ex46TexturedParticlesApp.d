63a231b508113424d86ee4c191570bbcab20cffe
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-06 05:05:49 -0700

    Chapter 04 fixes

--- Chapter04_ParticleSystems/Ex46TexturedParticles/xcode/build/Ex46TexturedParticles.build/Debug/Ex46TexturedParticles.build/Objects-normal/i386/Ex46TexturedParticlesApp.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter04_ParticleSystems/Ex46TexturedParticles/xcode/../src/Ex46TexturedParticlesApp.cpp \
-  ../../../../cinder_0.8.5_mac/include/cinder/app/AppNative.h \
-  ../../../../cinder_0.8.5_mac/include/cinder/gl/Texture.h \
-  ../../../../cinder_0.8.5_mac/include/cinder/Rand.h \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter04_ParticleSystems/Ex46TexturedParticles/xcode/../src/Particle.h
