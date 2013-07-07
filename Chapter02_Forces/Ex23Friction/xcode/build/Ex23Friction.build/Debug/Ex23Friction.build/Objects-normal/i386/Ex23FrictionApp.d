a023a9ba3cb7d29284873c11f093ef9bb008d7c0
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-07 02:57:44 -0700

    Chapter 02 done

--- Chapter02_Forces/Ex23Friction/xcode/build/Ex23Friction.build/Debug/Ex23Friction.build/Objects-normal/i386/Ex23FrictionApp.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter02_Forces/Ex23Friction/xcode/../src/Ex23FrictionApp.cpp \
-  ../../../../cinder_0.8.5_mac/include/cinder/app/AppNative.h \
-  ../../../../cinder_0.8.5_mac/include/cinder/Rand.h \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter02_Forces/Ex23Friction/xcode/../src/Mover.h
