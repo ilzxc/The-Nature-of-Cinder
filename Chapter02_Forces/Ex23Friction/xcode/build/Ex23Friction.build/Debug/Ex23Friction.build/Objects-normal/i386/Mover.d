599d2db54fa6e0aa818a9810353836f1488adf61
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-07 02:57:44 -0700

    Chapter 02 done

--- Chapter02_Forces/Ex23Friction/xcode/build/Ex23Friction.build/Debug/Ex23Friction.build/Objects-normal/i386/Mover.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter02_Forces/Ex23Friction/xcode/../src/Mover.cpp \
-  /Users/ilyarostovtsev/Documents/Creative\ Coding/The_Nature_of_Cinder/Chapter02_Forces/Ex23Friction/xcode/../src/Mover.h
