5d558764bb3f00f64bfe071729bb4035a1da178d
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-13 00:29:51 -0700

    Example 5.2

---
+++ Chapter05_PhysicsLibraries/Ex52FixedObjects/src/Box.cpp
@@ -0,0 +1,35 @@
+//
+//  Box.cpp
+//  Ex52FixedObjects
+//
+//  Created by Ilya Rostovtsev on 7/12/13.
+//
+//
+
+#include "Box.h"
+
+void Box::update(b2World * world){
+    position = Conversions::toScreen( body->GetPosition() );
+    angle = toDegrees( body->GetAngle() );
+}
+
+void Box::draw() const {
+    gl::color( 127.0f/255, 127.0f/255, 127.0f/255 );
+    gl::pushMatrices();
+    gl::translate( position );
+    gl::rotate( angle );
+    gl::drawSolidRect( bounds );
+    gl::popMatrices();
+}
+
+bool Box::isDead() {
+    if( position.y > app::getWindowHeight() ) {
+        return true;
+    }
+    
+    return false;
+}
+
+b2Body* Box::getBody() {
+    return body;
+}
