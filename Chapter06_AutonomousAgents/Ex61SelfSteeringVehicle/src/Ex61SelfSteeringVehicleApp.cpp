e9813d6075c2df29133beb158d67d928b6cef57e
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-15 11:15:25 -0700

    Example 6.1

---
+++ Chapter06_AutonomousAgents/Ex61SelfSteeringVehicle/src/Ex61SelfSteeringVehicleApp.cpp
@@ -0,0 +1,45 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "Vehicle.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class Ex61SelfSteeringVehicleApp : public AppNative {
+  public:
+	void setup();
+	void mouseMove( MouseEvent event );
+	void update();
+	void draw();
+    
+    Vehicle vehicle;
+    Vec2f mouse;
+    float mouseRadius;
+};
+
+void Ex61SelfSteeringVehicleApp::setup() {
+    vehicle = Vehicle( app::getWindowWidth(), app::getWindowHeight() );
+    mouse = Vec2f( app::getWindowCenter() );
+    mouseRadius = 50.0f;
+}
+
+void Ex61SelfSteeringVehicleApp::mouseMove( MouseEvent event ) {
+    mouse = event.getPos();
+}
+
+void Ex61SelfSteeringVehicleApp::update() {
+    vehicle.update( mouse );
+}
+
+void Ex61SelfSteeringVehicleApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0.9f, 0.9f, 0.9f ) );
+    
+    gl::color(0.444f, 0.444f, 0.444f);
+    gl::drawSolidCircle( mouse, mouseRadius );
+    vehicle.draw();
+}
+
+CINDER_APP_NATIVE( Ex61SelfSteeringVehicleApp, RendererGl )
