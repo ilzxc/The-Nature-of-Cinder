9f48fd7e77a116461e5fb08f2e3f7bb8e3ee728b
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-15 14:51:09 -0700

    Example 6.2

---
+++ Chapter06_AutonomousAgents/Ex62WanderingVehicle/src/Ex62WanderingVehicleApp.cpp
@@ -0,0 +1,38 @@
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "Vehicle.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class Ex62WanderingVehicleApp : public AppNative {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );	
+	void update();
+	void draw();
+    
+    Vehicle vehicle;
+};
+
+void Ex62WanderingVehicleApp::setup() {
+    vehicle = Vehicle( getWindowCenter() );
+}
+
+void Ex62WanderingVehicleApp::mouseDown( MouseEvent event ) {
+    vehicle.toggleDebug();
+}
+
+void Ex62WanderingVehicleApp::update() {
+    vehicle.update();
+}
+
+void Ex62WanderingVehicleApp::draw()
+{
+	// clear out the window with black
+	gl::clear( Color( 0.88f, 0.88f, 0.88f ) );
+    vehicle.draw();
+}
+
+CINDER_APP_NATIVE( Ex62WanderingVehicleApp, RendererGl )
