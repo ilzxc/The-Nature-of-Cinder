c83c0f4ed541bcf6f178669467556d17670733f9
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-15 11:17:45 -0700

    iOS Example

---
+++ ZZ_BonusWork/iOSTest/src/Repeller.cpp
@@ -0,0 +1,37 @@
+//
+//  Repeller.cpp
+//  Ex45ParticleRepeller
+//
+//  Created by Ilya Rostovtsev on 7/3/13.
+//
+//
+
+#include "Repeller.h"
+
+void Repeller::draw() {
+    gl::pushMatrices();
+    gl::translate(location);
+    gl::color(30.0/255, 95.0/255, 153.0/255);
+    gl::drawSolidCircle(Vec2f(0, 0), radius);
+    gl::color(102.0/255, 156.0/255, 204.0/255);
+    gl::drawSolidCircle(Vec2f(0, 0), radius * 0.80f);
+    gl::popMatrices();
+}
+
+Vec2f Repeller::repel( const Particle& p ) const {
+    Vec2f direction = location - p.getPosition();
+    float d = direction.length();
+    direction.normalize();
+    if (d < 15.0f) {
+        d = 15.0f;
+    } else if (d > radius * 2) {
+        d = radius * 2;
+    }
+    float force = -1.0f * strength / ( d * d );
+    direction *= force;
+    return direction;
+}
+
+void Repeller::setLocation( Vec2i mouse ) {
+    location = mouse;
+}
