632117581231ef772e96838d6409085bc2f4f63b
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-23 02:33:35 -0700

    Example 5.6

---
+++ Chapter05_PhysicsLibraries/Ex56Forces/src/Ex56ForcesApp.cpp
@@ -0,0 +1,80 @@
+#include <Box2D/Box2D.h>
+#include "cinder/app/AppNative.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Rand.h"
+#include "Attractor.h"
+#include "Particle.h"
+
+using namespace ci;
+using namespace ci::app;
+using namespace std;
+
+class Ex56ForcesApp : public AppNative {
+  public:
+	void setup();
+	void mouseDown( MouseEvent event );
+    void mouseUp( MouseEvent event );
+    void mouseMove( MouseEvent event );
+    void mouseDrag( MouseEvent event );
+	void update();
+	void draw();
+    
+    b2World *world;
+    float32 timeStep;
+    float32 velocityIterations;
+    int32 positionIterations;    
+    
+    Attractor attractor;
+    vector<Particle> particles;
+    Vec2f mouseLocation;
+};
+
+void Ex56ForcesApp::setup() {
+    world = new b2World( b2Vec2( 0.0f, 0.0f ) ); // no gravity in this example!
+    timeStep = 1.0f / 60.0f;
+    velocityIterations = 8;
+    positionIterations = 3;
+    
+    attractor = Attractor( world, getWindowCenter().x, getWindowCenter().y );
+    for (int i = 0; i < 100; ++i) {
+        particles.push_back( Particle(world, Vec2f( Rand::randFloat( getWindowWidth() ), Rand::randFloat( getWindowHeight() ) ), randFloat( 7.0f, 14.0f ) ) );
+    }
+}
+
+void Ex56ForcesApp::mouseDown( MouseEvent event ) {
+    attractor.clicked( event.getPos() );
+}
+
+void Ex56ForcesApp::mouseUp( MouseEvent event ) {
+    attractor.stopDragging();
+}
+
+void Ex56ForcesApp::mouseMove( MouseEvent event ) {
+    mouseLocation = event.getPos();
+}
+
+void Ex56ForcesApp::mouseDrag( MouseEvent event ) {
+    mouseLocation = event.getPos();
+}
+
+void Ex56ForcesApp::update() {
+    world->Step( timeStep, velocityIterations, positionIterations );
+    attractor.hover( mouseLocation );
+    attractor.drag( mouseLocation );
+    
+    for ( auto& particle : particles ) {
+        particle.update( world, attractor.attract( particle ) );
+    }
+}
+
+void Ex56ForcesApp::draw() {
+	gl::clear( Color( 0, 0, 0 ) );
+    
+    attractor.draw();
+    
+    for ( auto& particle : particles ) {
+        particle.draw();
+    }
+}
+
+CINDER_APP_NATIVE( Ex56ForcesApp, RendererGl )
--- Chapter05_PhysicsLibraries/Ex56Forces/src/Particle.cpp
+++ Chapter05_PhysicsLibraries/Ex56Forces/src/Particle.cpp
@@ -7,3 +7,57 @@
 //
 
 #include "Particle.h"
+
+Particle::Particle( b2World *world, const Vec2f &_position, float _radius )
+: position( _position ),
+  radius( _radius ){
+      b2BodyDef bd;
+      bd.position = Conversions::toPhysics( position );
+      bd.angle = toRadians( angle );
+      bd.type = b2_dynamicBody;
+      body = world->CreateBody( &bd );
+      
+      b2CircleShape cs;
+      cs.m_radius = Conversions::toPhysics( radius );
+      b2FixtureDef fd;
+      fd.shape = &cs;
+      fd.density = 1.0f;
+      fd.friction = 0.03f;
+      fd.restitution = 0.3f;
+      
+      body->CreateFixture( &fd );
+}
+
+void Particle::update( b2World * world, const b2Vec2& force ) {
+    body->ApplyForceToCenter( force );
+    position = Conversions::toScreen( body->GetPosition() );
+    angle = toDegrees( body->GetAngle() );
+}
+
+void Particle::draw() const {
+    gl::pushMatrices();
+    gl::translate( position );
+    gl::rotate( angle );
+    gl::color( 0.221, 0.221, 0.221 );
+    gl::drawSolidCircle( Vec2f::zero(), radius, 16 );
+    gl::color( 0.721, 0.721, 0.721 );
+    gl::drawSolidCircle( Vec2f::zero(), radius * 0.7f, 16 );
+    gl::color( 0.221, 0.221, 0.221 );
+    gl::drawSolidRect( Rectf(0, -2, radius, 2) );
+    gl::popMatrices();
+}
+
+b2Body* Particle::getBody() const {
+    return body;
+}
+
+void Particle::killBody( b2World * world ) {
+    world->DestroyBody( body );
+}
+
+bool Particle::isDead() const {
+    if ( position.y > app::getWindowHeight() + (radius * 2) ) {
+        return true;
+    }
+    return false;
+}
