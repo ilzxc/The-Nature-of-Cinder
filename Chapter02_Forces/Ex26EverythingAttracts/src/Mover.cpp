86622512571fb396fbdb71756a9363851cc28c6f
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-07-24 20:18:50 -0700

    housekeeping updates

--- Chapter02_Forces/Ex26EverythingAttracts/src/Mover.cpp
+++ Chapter02_Forces/Ex26EverythingAttracts/src/Mover.cpp
@@ -16,21 +16,28 @@ void Mover::applyForce( const Vec2f& force ) {
 void Mover::update() {
     velocity += acceleration;
     location += velocity;
+    lastAcceleration = acceleration;
     acceleration = Vec2f::zero();
 }
 
 void Mover::draw() {
     gl::color( 1, 1, 1 );
-    gl::drawSolidCircle( location, radius );
+    gl::pushMatrices();
+    gl::translate( location );
+    gl::color( 1, 1, 1, 0.4 );
+    gl::drawLine( Vec2f::zero(), lastAcceleration * 150 );
+    gl::color( 1, 1, 1 );
+    gl::drawSolidCircle( Vec2f::zero(), radius );
+    gl::popMatrices();
 }
 
 Vec2f Mover::attract( const Mover& m, const float g ) {
     Vec2f force = location - m.getLocation();
-    float distance = force.length();
-    distance = (distance < 5.0f) ? 5.0f : ( (distance > 25.0f) ? 25.0 : distance );
+    float distance = force.lengthSquared();
+    distance = (distance < 25.0f) ? 25.0f : ( (distance > 625.0f) ? 625.0 : distance );
     force.normalize();
     
-    float strength = g * mass * m.getMass() / (distance * distance);
+    float strength = g * mass * m.getMass() / (distance);
     force *= strength;
     return force;
 }
