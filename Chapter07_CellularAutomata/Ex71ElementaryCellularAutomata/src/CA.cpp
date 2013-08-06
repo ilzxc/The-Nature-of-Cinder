b55e1498a47f81ce8e1adceaabfea506bdd19751
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-08-06 05:21:25 -0700

    removing incomplete exercies

--- Chapter07_CellularAutomata/Ex71ElementaryCellularAutomata/src/CA.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  CA.cpp
-//  Ex71ElementaryCellularAutomata
-//
-//  Created by Ilya Rostovtsev on 8/4/13.
-//
-//
-
-#include "CA.h"
--- ZZ_BonusWork/Box2DFragileShapes/src/FragileSquare.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  FragileSquare.cpp
-//  Box2DFragileShapes
-//
-//  Created by Ilya Rostovtsev on 7/28/13.
-//
-//
-
-#include "FragileSquare.h"
