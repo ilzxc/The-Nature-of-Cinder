5e87db67111e6a38a2bcd3acb4fe7ba439da3d5e
Ilya Y. Rostovtsev <ilyaforpresident@gmail.com>
2013-08-06 05:21:25 -0700

    removing incomplete exercies

--- ZZ_BonusWork/Box2DArm/src/Arm.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Arm.cpp
-//  Box2DArm
-//
-//  Created by Ilya Rostovtsev on 8/3/13.
-//
-//
-
-#include "Arm.h"
--- ZZ_BonusWork/Box2DFragileShapes/src/Shard.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Shard.cpp
-//  Box2DFragileShapes
-//
-//  Created by Ilya Rostovtsev on 7/29/13.
-//
-//
-
-#include "Shard.h"
